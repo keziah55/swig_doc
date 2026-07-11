9e515d29b110b8263814fa2f2e85a24571460c58
keziah55 <19816994+keziah55@users.noreply.github.com>
2026-07-11 16:53:28 +0100

    tidying up

--- tests/test_md_utils.py
+++ tests/test_md_utils.py
@@ -1,81 +1,9 @@
 import json
 
-# from bs4 import BeautifulSoup
-
 from swig_doc.html_parser import HtmlPageParser
-from swig_doc.md_utils import MarkdownFormatter
 from swig_doc.exceptions import ParsingException
 
 import pytest
-
-
-# @pytest.mark.parametrize("code_type", ["targetlang", "code", "shell", "diagram"])
-# def test_code_block(data_dir, code_type):
-
-#     fname = "code_blocks"
-
-#     html = data_dir.joinpath(f"{fname}.html").read_text()
-
-#     with open(data_dir.joinpath(f"{fname}.json")) as fileobj:
-#         md = json.load(fileobj)
-
-#     soup = BeautifulSoup(html, "html.parser")
-
-#     for tag in soup.find_all("div", attrs={"class_": code_type}):
-#         expected = md[code_type]
-#         assert MarkdownFormatter.code_block(tag, target_language="python") == expected
-
-
-# @pytest.mark.parametrize(
-#     "html,err_msg",
-#     [
-#         ("<div></div>", r"Could not get content of 'pre' tag in:\n<div></div>"),
-#         (
-#             '<div class="a b"><pre>demo</pre></div>',
-#             r"Multiple classes in tag; cannot determine code language",
-#         ),
-#         (
-#             '<div class=""><pre>demo</pre></div>',
-#             r"No code language class in",
-#         ),
-#     ],
-# )
-# def test_code_block_error(html, err_msg):
-
-#     soup = BeautifulSoup(html, "html.parser")
-#     tag = soup.find("div")
-
-#     with pytest.raises(ParsingException, match=err_msg):
-#         MarkdownFormatter.code_block(tag, target_language="python")
-
-
-# def test_make_md_head():
-#     assert MarkdownFormatter.make_md_head("Heading", level=4) == "#### Heading"
-
-
-# @pytest.mark.parametrize(
-#     "html, expected",
-#     [
-#         ('<h4><a name="Python">Python</a></h4>', '#### <a name="Python"></a> Python'),
-#         ("<h1>Python</h1>", "# Python"),
-#     ],
-# )
-# def test_header(html, expected):
-
-#     soup = BeautifulSoup(html, "html.parser")
-#     tag = list(soup.descendants)[0]
-
-#     assert MarkdownFormatter.header(tag) == expected
-
-
-# def test_header_error():
-
-#     html = "<h>Python<h>"
-#     soup = BeautifulSoup(html, "html.parser")
-#     tag = soup.find("h")
-
-#     with pytest.raises(ParsingException, match=r"Cannot get header level from 'h'"):
-#         MarkdownFormatter.header(tag)
 
 
 def test_convert_text_format(data_dir):
@@ -91,7 +19,6 @@ def test_convert_text_format(data_dir):
     print(md)
 
     assert md == expected
-
 
     # assert MarkdownFormatter.convert_text_format(html) == expected
 
