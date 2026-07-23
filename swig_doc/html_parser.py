643b28fd28a23546191ddd309f0d88e6d40adb08
keziah55 <19816994+keziah55@users.noreply.github.com>
2026-07-23 22:03:30 +0100

    rename/move classes

--- swig_doc/html_parser.py
+++ swig_doc/html_parser.py
@@ -1,116 +1,13 @@
 from html.parser import HTMLParser
-from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Optional, Self
+from typing import Optional
 import warnings
 import re
 
-from .md_utils import HtmlToMd, HEADER_REGEX
-from .tag_stack import TagStack
+
+from .doc_item import DocumentItem, DocItemStack
+from .md_utils import HtmlToMd
 from .exceptions import EndTagWarning
-
-
-@dataclass
-class DocumentItem:
-    """Class representing document item, either a block item or inline item."""
-
-    tag: str
-    doc_pos: tuple[int, int]
-    data: list[str] = field(default_factory=list)
-    attrs: Optional[dict] = None
-    parent: Optional[Self] = None
-
-    _quiet: bool = True
-
-    _block_tags = ["p", "div", "ul", "ol", "blockquote"]  # also headers
-
-    def __repr__(self) -> str:
-
-        s = f"'{self.tag}' at pos {self.doc_pos}"
-        if self.attrs:
-            s += ": "
-            s += " ".join([f'{k}="{v}"' for k, v in self.attrs.items()])
-
-        return s
-
-    @property
-    def is_block(self) -> bool:
-        """
-        Return True if this DocumentItem represents a block of text.
-
-        False if it represents an inline item.
-        """
-        return self.tag in self._block_tags or self.is_header
-
-    @property
-    def is_header(self) -> bool:
-        """Return True if this DocumentItem represents a header tag."""
-        return HEADER_REGEX.match(self.tag) is not None
-
-    def to_str(self) -> str:
-        """Return markdown string of this `DocumentItem`."""
-
-        if self.tag == "a":
-            return self._link_to_str()
-
-        if self.data is None:
-            return ""
-
-        if not self._quiet and self.tag in ["ol", "ul"]:
-            print(f"{self.tag}: {self.data}")
-
-        s = "".join(self.data)
-
-        if self.tag in ["li"]:
-            # s = s.lstrip()
-            s = s.rstrip()
-        if self.tag in ["p"]:
-            s = s.strip()
-
-        # if re.match(r"h\d+", self.tag):
-        #     s = re.sub(r"\n", "", s)
-        # s = f"\n{s}\n"
-
-        if self.is_block:
-            ret = f"\n{s}\n"
-        elif self.tag == "li":
-            ret = f"{s}\n"
-        else:
-            ret = s
-
-        return ret
-
-    def _link_to_str(self) -> str:
-        """Return markdown string representation of link/anchor."""
-
-        anchor = self.attrs.get("name", None)
-        url = self.attrs.get("href", None)
-
-        title = "".join(self.data)
-
-        if title is not None and anchor is not None:
-            s = f'<a name="{anchor}"></a> {title}'
-        elif title is not None and url is not None:
-            s = f"[{title}]({url})"
-        else:
-            raise Exception(
-                f"LinkItem requires title and either anchor or url\n"
-                f"{title=}, {url=}, {anchor=}"
-            )
-
-        s = re.sub(r"\n", " ", s)
-        return s
-
-    def append_data(self, data: Optional[str]):
-        """Add more `data` to this item."""
-
-        if len(self.data) == 1 and self.tag in ["li"]:  # ["ul", "ol"]:
-            data = data.lstrip()
-
-        if data is None or data == "":
-            return
-
-        self.data.append(data)
 
 
 class HtmlPageParser(HTMLParser):
@@ -139,7 +36,7 @@ class HtmlPageParser(HTMLParser):
         self._doc_parts: list[str] = []
 
         # list of open DocumentItems
-        self._doc_items = TagStack()
+        self._doc_items = DocItemStack()
 
         self._active = True
 
