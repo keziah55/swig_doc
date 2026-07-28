11c877152b116fafafc7d1940674e9cd4d5e762d
keziah55 <19816994+keziah55@users.noreply.github.com>
2026-07-28 21:11:43 +0100

    _validate function

--- swig_doc/swig_doc_parser.py
+++ swig_doc/swig_doc_parser.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from typing import Optional
+import re
+
+from bs4 import BeautifulSoup
 
 from .html_parser import HtmlPageParser
 from .md_utils import make_md_head
@@ -65,11 +68,26 @@ class SwigDocParser:
 
         self._write_index()
 
+        validation: dict[str : dict[int, set]] = {}
+
         for name in self._chapters:
-            # if name != "Windows":
+            # if name != "Extending":
             #     continue
-            self._write_file(name)
+            md = self._make_markdown(name)
+            self._write_file(name, md)
             self._write_redirect_page(name)
+
+            if (diff := self._validate(name, md)) is not None:
+                validation[name] = diff
+
+        if validation:
+            print("The following pages failed header validation:")
+            for name, diff in validation.items():
+                print(f"{name}:")
+                for level, headers in diff.items():
+                    print(f"  Header level {level}")
+                    for header in headers:
+                        print(f"    {header}")
 
     def _get_chapters_list(self) -> list[str]:
 
@@ -91,8 +109,8 @@ class SwigDocParser:
 
         index_file.write_text("\n".join(lines))
 
-    def _write_file(self, name: str):
-        """Write markdown file from html page for the given name."""
+    def _make_markdown(self, name: str) -> str:
+        """Generate markdown text from html page for the given name."""
 
         html_file = self._html_path.joinpath(f"{name}.html")
 
@@ -107,8 +125,12 @@ class SwigDocParser:
         parser = HtmlPageParser(target_language=language, shell_language=shell_language)
         text = parser.parse(html_file)
 
-        out_file = self._out_path.joinpath(f"{name}{MARKDOWN_EXT}")
+        return text
 
+    def _write_file(self, name: str, text: str):
+        """Write markdown file."""
+
+        out_file = self._out_path.joinpath(f"{name}{MARKDOWN_EXT}")
         out_file.write_text(text)
 
     def _write_redirect_page(self, name: str):
@@ -140,3 +162,38 @@ class SwigDocParser:
             # page are for mingw/cygwin
             return "powershell"
         return None
+
+    def _validate(self, name: str, md: str) -> Optional[dict[int:set]]:
+        """
+        Check that headers in html file match those in markdown text.
+
+        Return `None` if there are no differences. Otherwise, return a dict of header level (int)
+        and set of non-matching header strings.
+        """
+
+        html_file = self._html_path.joinpath(f"{name}.html")
+        soup = BeautifulSoup(html_file.read_text(), "html.parser")
+
+        diff = {}
+
+        for header_level in range(1, 7):
+            html_headers = [
+                (tag.string, tag.a["name"]) for tag in soup.find_all(f"h{header_level}")
+            ]
+
+            md_head = f"#{{{header_level}}}"
+            md_headers = [
+                # un-escape any `>` chars in header string
+                (m.group("h_str").replace("\\>", ">"), m.group("a_name"))
+                for m in re.finditer(
+                    r"\n" + md_head + r' <a name="(?P<a_name>\w+)"></a> +(?P<h_str>.+)', md
+                )
+            ]
+
+            if html_headers != md_headers:
+                diff[header_level] = set(html_headers) ^ set(md_headers)
+
+        if diff:
+            return diff
+        else:
+            return None
