50bcbd0d8fee497717bad2fe96dff416f4333ca3
keziah55 <19816994+keziah55@users.noreply.github.com>
2026-08-21 16:47:50 +0100

    include window.location.hash in js redirect; resovles #26

--- swig_doc/utils.py
+++ swig_doc/utils.py
@@ -13,7 +13,7 @@ REDIRECT_TEMPLATE = Template(
         <meta charset="UTF-8">
         <meta http-equiv="refresh" content="0; url=$url">
         <script type="text/javascript">
-            window.location.href = "$url"
+            window.location.href = "$url" + window.location.hash
         </script>
         <title>Page Redirection</title>
     </head>
